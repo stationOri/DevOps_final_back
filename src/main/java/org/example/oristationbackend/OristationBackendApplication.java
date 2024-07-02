c31ab18245a6de27d17bb309a6323ac6a859b2ec
myungji2 <myungji129@naver.com>
2024-07-02 17:17:03 +0900

    first commit

---
+++ src/main/java/org/example/oristationbackend/OristationBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.oristationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OristationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OristationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/oristationbackend/OristationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.oristationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OristationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
