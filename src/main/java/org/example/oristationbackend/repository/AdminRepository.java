aba5f4e6ab86481530c717b739f26b34b6eb1dd8
starht <star2thb@gmail.com>
2024-07-09 17:47:16 +0900

    Docs: 이름 변경

--- src/main/java/org/example/oristationbackend/repository/AdminRepository.java
+++ src/main/java/org/example/oristationbackend/repository/AdminRepository.java
@@ -1,6 +1,7 @@
 package org.example.oristationbackend.repository;
 
 import org.example.oristationbackend.entity.Admin;
+import org.example.oristationbackend.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
