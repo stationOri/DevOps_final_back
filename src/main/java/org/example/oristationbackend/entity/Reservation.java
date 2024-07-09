aec0cf499cd96062aebfed90c0db4da915ccaba8
starht <star2thb@gmail.com>
2024-07-09 15:08:19 +0900

    build

--- src/main/java/org/example/oristationbackend/entity/Reservation.java
+++ src/main/java/org/example/oristationbackend/entity/Reservation.java
@@ -1,4 +1,10 @@
 package org.example.oristationbackend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Reservation {
+  @Id
+  private int reservationId;
 }
