96241be1efb9267dbccff8fd0e643f34c2ac3d67
myungji2 <myungji129@naver.com>
2024-07-09 13:22:08 +0900

    res interface

--- src/main/java/org/example/oristationbackend/repository/reservationRepository.java
+++ src/main/java/org/example/oristationbackend/repository/reservationRepository.java
@@ -1,4 +1,7 @@
 package org.example.oristationbackend.repository;
 
-public class reservationRepository {
+import org.example.oristationbackend.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface reservationRepository extends JpaRepository<Reservation, Integer> {
 }
