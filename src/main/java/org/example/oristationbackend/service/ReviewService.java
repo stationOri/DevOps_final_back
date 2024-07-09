b51293090d4b900891d619027b42f2eb17251240
starht <star2thb@gmail.com>
2024-07-09 17:42:01 +0900

    Feat: Service 초기 생성

---
+++ src/main/java/org/example/oristationbackend/service/ReviewService.java
@@ -0,0 +1,11 @@
+package org.example.oristationbackend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ReviewService {
+}
--- src/main/java/org/example/oristationbackend/service/reservationService.java
+++
@@ -1,4 +0,0 @@
-package org.example.oristationbackend.service;
-
-public class reservationService {
-}
