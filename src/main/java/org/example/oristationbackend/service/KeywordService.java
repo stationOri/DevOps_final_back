6745956a8f2554c801b766acd0c017968ab9fc0f
starht <star2thb@gmail.com>
2024-07-15 13:22:15 +0900

    Feat: 키워드 전체 GET

--- src/main/java/org/example/oristationbackend/service/KeywordService.java
+++ src/main/java/org/example/oristationbackend/service/KeywordService.java
@@ -1,11 +1,20 @@
 package org.example.oristationbackend.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.oristationbackend.entity.Keyword;
+import org.example.oristationbackend.repository.KeywordRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class KeywordService {
+  private final KeywordRepository keywordRepository;
+
+  public List<Keyword> getAllKeywords() {
+    return keywordRepository.findAll();
+  }
 }
