2789a30709159e4335a769fa42b208785c8d8114
01022549653@naver.com <>
2024-07-24 15:00:52 +0900

    feat: 성수기 등록, 조회, 삭제

--- src/main/java/org/example/oristationbackend/dto/restaurant/PeakListResDto.java
+++ src/main/java/org/example/oristationbackend/dto/restaurant/PeakListResDto.java
@@ -1,4 +1,18 @@
 package org.example.oristationbackend.dto.restaurant;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PeakListResDto {
+    private int peakId; // 성수기 예약 id
+    private String dateStart; // 성수기 시작 날짜
+    private String dateEnd; // 성수기 마감 날짜
+    private String peakOpendate; //성수기 예약 오픈 날짜
+    private int restId; //식당 ID
 }
