0269346df8807950ad182d3943940954d8e32971
starht <star2thb@gmail.com>
2024-07-25 18:26:56 +0900

    Feat: 관리자 예약 취소 enum 추가

--- src/main/java/org/example/oristationbackend/entity/type/ReservationStatus.java
+++ src/main/java/org/example/oristationbackend/entity/type/ReservationStatus.java
@@ -8,6 +8,7 @@ public enum ReservationStatus {
     RESERVATION_REJECTED("예약거절"),
     RESERVATION_CANCELED_BYREST("예약취소(식당)"),
     RESERVATION_CANCELED_BYUSER("예약취소(사용자)"),
+    RESERVATION_CANCELED_BYADMIN("예약취소(관리자)"),
     VISITED("방문"),
     NOSHOW("노쇼");
 
