b1d2d308106ba38769c5f7c7e3eda35d60964741
starht <star2thb@gmail.com>
2024-07-29 11:53:25 +0900

    Feat: localhost config 추가

--- src/main/java/org/example/oristationbackend/config/WebConfig.java
+++ src/main/java/org/example/oristationbackend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("https://waitmate.shop")
+        .allowedOrigins("https://waitmate.shop", "http://localhost:3000") // localhost:3000 추가
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
